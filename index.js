a1d41287770848c0bf1125026127f5185cbca951
Rodolfo De Chiara <rodolfo.dechiara@tuta.io>
2022-06-26 15:35:29 +0200

    ref: remove unused variable

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ document.querySelector('form').addEventListener('submit', (e) =>
     const pwd1 = document.getElementById('password1');
     const pwd2 = document.getElementById('password2');
     const errMsg = document.getElementById('err-msg');
-    const label1 = document.getElementById('lbl-pwd1');
-
     if(pwd1.value !== pwd2.value)
     {
         e.preventDefault();
